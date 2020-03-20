77582d026604dfaea0165b9c214843c712993d60
trajanme <shintaro@trajan.me>
2020-03-20 22:23:45 +0900

    Initial Commit

---
+++ swift-tableview-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-tableview-sample
+//
+//  Created by Shintaro Ishikawa on 2020/03/20.
+//  Copyright © 2020 trajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
