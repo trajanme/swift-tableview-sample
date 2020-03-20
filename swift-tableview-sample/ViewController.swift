dad2500642ea4159053897829d40a2af1a78e029
trajanme <shintaro@trajan.me>
2020-03-21 00:30:09 +0900

    added tableview

--- swift-tableview-sample/ViewController.swift
+++ swift-tableview-sample/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // tableに表示するセル数を返す
+        return tables.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // storyboardに設定したidentifierをもとに、ソース側でテーブルのセルごとにデータ設定していく
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // 各セルに変数tablesの値を表示できるように設定する
+        cell.textLabel?.text = tables[indexPath.row]
+        
+        return cell
+    }
+    
+    var tables:[String] = ["Augustus", "Tiberius", "Caligula", "Claudius", "Nero"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // tableViewCellをこのViewで使うことを宣言する
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    // storyboardからTableViewを紐付ける
+    @IBOutlet weak var tableView: UITableView!
+    
 }
 
